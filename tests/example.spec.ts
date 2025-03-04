366bf70d8c9708f9b2f4cce53a7b7851f1cef2a5
Luis Enrique Munoz <le.bmunoz@gmail.com>
2025-03-04 12:49:24 -0300

    First challenge draft passing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Check Powr pricing page availability', async ({ page }) => {
+  await page.goto('https://www.powr.io/pricing')
+  await page.locator('.Select-value').click();
+  await page.getByLabel('Social Feed').getByText('Social Feed').click();
+  await expect(page.getByRole('option', { name: 'Social Feed' })).toBeVisible();
+  await page.locator('div:nth-child(3) > .toggle > .toggle__container > .toggle__switcher').click();
+  await expect(page.getByRole('heading', { name: 'Free' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '$0' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Starter' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '$5' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Pro', exact: true })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '$13' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Business' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '$89' })).toBeVisible();
+  await page.locator('div:nth-child(3) > .toggle > .toggle__container > .toggle__switcher').click();
+  await expect(page.getByText('Annually', { exact: true }).nth(1)).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Free' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '$0' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Starter' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '$4' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Pro', exact: true })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '$12' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Business' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '$80' })).toBeVisible();
 });
