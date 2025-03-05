18bc7563c6a0b64601ea70a380ffcbfb2474fb1a
Luis Enrique Munoz <le.bmunoz@gmail.com>
2025-03-05 18:41:45 -0300

    ci test

--- tests/login.setup.ts
+++ tests/login.setup.ts
@@ -6,6 +6,8 @@ setup('setup login auth data', async ({ page }) => {
   await page.locator('#sign_in_email').fill(process.env.USER as string);
   await page.locator('#sign_in_email').press('Tab');
   await page.locator('#new_sign_in_password').fill(process.env.PASS as string);
+  await expect(page.locator('#sign_in_email')).toHaveValue('le.bmunoz+user1@gmail.com');
+  await expect(page.locator('#new_sign_in_password')).toHaveValue('Test@1234');
   await page.locator('#sign-in-submit').click();
   await expect(page.locator('.nav-logo')).toBeVisible();
   await expect(page.getByRole('heading', { name: 'POWR Dashboard' })).toBeVisible();
